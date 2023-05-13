dd606e65e5913cd33a5dd4ccfe1114bfe611883c
LucaBrivio183 <l.brivio4@gmail.com>
2023-05-13 11:26:24 +0200

    feat: validation, and edited messages

--- resources/views/comic/create.blade.php
+++ resources/views/comic/create.blade.php
@@ -16,51 +16,74 @@
             {{-- title --}}
             <div class="mb-3">
                 <label for="title" class="form-label">Titolo</label>
-                <input type="text" class="form-control" id="title" name="title">
+                <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{ old('title') }}">
+                @error('title')
+                    <div class="alert alert-danger">{{ $message }} </div>
+                @enderror
             </div>
             {{-- description --}}
             <div class="mb-3">
                 <label for="description" class="form-label">Descrizione</label>
-                <textarea class="form-control" id="description" name="description"></textarea>
+                <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description"> {{ old('description') }}</textarea>
+                @error('description')
+                    <div class="alert alert-danger">{{ $message }} </div>
+                @enderror
             </div>
             {{-- thumb --}}
             <div class="mb-3">
                 <label for="thumb" class="form-label">Immagine</label>
-                <input type="text" class="form-control" id="thumb" name="thumb">
+                <input type="text" class="form-control @error('thumb') is-invalid @enderror" id="thumb" name="thumb" value="{{ old('thumb') }}">
+                @error('thumb')
+                    <div class="alert alert-danger">{{ $message }} </div>
+                @enderror
             </div>
             {{-- price --}}
             <div class="mb-3">
                 <label for="price" class="form-label">Prezzo</label>
-                <input type="text" class="form-control" id="price" name="price">
+                <input type="text" class="form-control @error('price') is-invalid @enderror" id="price" name="price" value="{{ old('price') }}">
+                @error('price')
+                    <div class="alert alert-danger">{{ $message }} </div>
+                @enderror
             </div>
             {{-- series --}}
             <div class="mb-3">
                 <label for="series" class="form-label">Serie</label>
-                <input type="text" class="form-control" id="series" name="series">
+                <input type="text" class="form-control @error('series') is-invalid @enderror" id="series" name="series" value="{{ old('series') }}">
+                @error('series')
+                    <div class="alert alert-danger">{{ $message }} </div>
+                @enderror
             </div>
             {{-- sale_date --}}
             <div class="mb-3">
                 <label for="sale_date" class="form-label">Data</label>
-                <input type="date" class="form-control" id="sale_date" name="sale_date">
+                <input type="date" class="form-control @error('sale_date') is-invalid @enderror" id="sale_date" name="sale_date"  value="{{ old('sale_date') }}">
+                @error('sale_date')
+                    <div class="alert alert-danger">{{ $message }} </div>
+                @enderror
             </div>
             {{-- type --}}
             <div class="mb-3">
                 <label for="type" class="form-label">Tipo</label>
                 <select class="form-select" id="type" name="type">
-                    <option selected>Scegli il tipo</option>
-                    <option value="comic-book">comic book</option>
-                    <option value="graphic-novel">graphic novel</option>
+                    <option value="comic-book" {{ old('type') === 'comic book' ? 'selected' : null }}>comic book</option>
+                    <option value="graphic-novel" {{ old('type') === 'graphic novel' ? 'selected' : null }}>graphic novel</option>
                 </select>
             </div>
             {{-- artists --}}
             <div class="mb-3">
                 <label for="artists" class="form-label">Artisti</label>
-                <input type="text" class="form-control" id="artists" name="artists">
+                <input type="text" class="form-control @error('artists') is-invalid @enderror" id="artists" name="artists" value="{{ old('artists') }}">
+                @error('artists')
+                    <div class="alert alert-danger">{{ $message }} </div>
+                @enderror
             </div>
             {{-- writers --}}
             <div class="mb-3">
                 <label for="writers" class="form-label">Scrittori</label>
-                <input type="text" class="form-control" id="writers" name="writers">
+                <input type="text" class="form-control @error('writers') is-invalid @enderror" id="writers" name="writers" value="{{ old('writers') }}">
+                @error('writers')
+                    <div class="alert alert-danger">{{ $message }} </div>
+                @enderror
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>   
